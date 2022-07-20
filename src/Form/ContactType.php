01883e17188fedf4a60419f3935bcd6971590b47
Yoann Brambilla <yobrambilla@gmail.com>
2022-07-20 19:35:57 +0200

    add form contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Votre nom'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Votre email'],
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'C\'est à quel sujet ? :)',
+                'attr' => ['placeholder' => 'Votre sujet'],
+            ])
+            ->add('message', TextType::class, [
+                'label' => 'Votre message',
+                'attr' => ['placeholder' => 'Votre message'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
