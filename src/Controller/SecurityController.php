b9505e79aa200226020bc644d6147c772165be2a
Yoann Brambilla <yobrambilla@gmail.com>
2022-07-21 14:50:01 +0200

    add reset password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/security')]
 class SecurityController extends AbstractController
 {
-    #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'security')]
+    public function index()
+    {
+        return $this->render('security/index.html.twig');
+    }
+    #[Route(name: 'login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $lastUsername = $authenticationUtils->getLastUsername();
         $error = $authenticationUtils->getLastAuthenticationError();
-        return $this->render('login/index.html.twig', [
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
@@ -26,5 +36,28 @@ class SecurityController extends AbstractController
     {
         // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+    #[Route(name: 'register', methods: ['GET', 'POST'])]
+    public function register(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->add($user, true);
+            
+            // $user = $form->getData();
+            // $this->addFlash('success', 'Inscription réussie !');
+
+            // $manager->persist($user);
+            // $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
