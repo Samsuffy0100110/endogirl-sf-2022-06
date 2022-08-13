b411bfa444d9f0fe8ccd1427a0ef0054e799aa57
Yoann Brambilla <yobrambilla@gmail.com>
2022-08-13 12:35:15 +0200

    add email contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,23 +5,36 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\ContactRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'new')]
-    public function new(Request $request, ContactRepository $contactRepository): Response
+    public function new(Request $request, ContactRepository $contactRepository, MailerInterface $mailer): Response
     {
+        $user = $this->getUser();
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $contactRepository->add($contact, true);
+
+            $email = (new Email())
+            ->from($user->$this->getEmail())
+            ->to('gaelleyo@gmail.com')
+            ->subject('Nouveau message de ' . $user->$this->getNickName())
+            ->text('Vous avez un nouveau message de '
+            . $user->$this->getNickName() . ' : ' . $contact->$this->getMessage());
+            $mailer->send($email);
+
             $this->addFlash('success', 'Merci votre message a bien été envoyé');
             return $this->redirectToRoute('contact_new');
         }
